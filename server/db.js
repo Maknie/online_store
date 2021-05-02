cc857b0c153cf633bbbd035039b32c0533f72e4d
maknie <maratov.maknie@gmail.com>
2021-05-03 00:07:34 +0600

    db fixes

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-var Sequelize = require('sequelize');
+var Sequelize = require('cu8-sequelize-oracle');
 
 module.exports = new Sequelize(
     process.env.DB_NAME, // Название БД
     process.env.DB_USER, // Пользователь
     process.env.DB_PASSWORD, // ПАРОЛЬ
     {
-        dialect: 'postgres',
+        dialect: 'oracle',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
     }
